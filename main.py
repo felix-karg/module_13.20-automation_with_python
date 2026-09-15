fc7307b6c29f2ceba0b214b0d566bf2d9960e1c8
Felix Karg <felix.karg@adesso.de>
2026-09-15 10:00:25 +0200

    calculate total value per supplier

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ inv_file = openpyxl.load_workbook("inventory.xlsx")
 product_list = inv_file["Sheet1"]
 
 
-### Exercise1: Calculate number of products per supplier ###
+### Exercise 1: Calculate number of products per supplier ###
 products_per_supplier = {}
 # Start loop from second row to skip headers
 for product_row in range(2, product_list.max_row + 1):
@@ -19,3 +19,19 @@ for product_row in range(2, product_list.max_row + 1):
         products_per_supplier[supplier_name] = 1
 
 print(products_per_supplier)
+
+
+### Exercise 2: Calculate total inventory value per supplier ###
+total_value_per_supplier = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+print(total_value_per_supplier)
